25d4ec082972982c54d32ee822e9182a043ffeae
unknown <salhasalsabillatogubu@gmail.com>
2025-08-31 21:25:39 +0900

    abel_project

---
+++ salha_project/bin/salha_project.dart
@@ -0,0 +1,5 @@
+import 'package:salha_project/salha_project.dart' as salha_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${salha_project.calculate()}!');
+}
---
+++ salha_project/test/salha_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:salha_project/salha_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
